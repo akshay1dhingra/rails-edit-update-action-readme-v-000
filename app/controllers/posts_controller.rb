9d6f20a3f287ce251aba61ffe80ada9f09b0b36c
Akshay Dhingra <akshay1dhingra@gmail.com>
2018-11-06 19:14:26 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,13 +20,14 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
-  end
+     @post = Post.find(params[:id])
+   end
 
-  def update
-    @post = Post.find(params[:id])
-    @post.update(title: params[:title], description: params[:description])
-    redirect_to post_path(@post)
-  end
+   def update
+     @post = Post.find(params[:id])
+     @post.update(title: params[:post][:title], description: params[:post][:description])
+     redirect_to post_path(@post)
+   end
+
 
 end
